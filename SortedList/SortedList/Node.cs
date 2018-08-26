81dbc5e13a115d89ee069db92d3f2839d3659851
GMRDL1 <gmrstudent@outlook.com>
2018-08-26 16:27:30 -0700

    Worked on Node and Add.

---
+++ SortedList/SortedList/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Next;
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+        public Node()
+        {
+
+        }
+    }
+}
---
+++ SortedList/SortedList/SortedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    class SortedList
+    {
+        Node<int> Head;
+        public int Count;
+        public SortedList(int value)
+        {
+            Head = new Node<int>(value);
+            Count = 1;
+        }
+        public SortedList()
+        {
+            Head = new Node<int>();
+            Count = 0;
+        }
+        public void Add(int value)
+        {
+            if (Count == 0)
+            {
+                Head.Value = value;
+            }
+            else
+            {
+                for (int i = 0; i <= Count; i++)
+                {
+                    if(i = Count)
+                }
+            }
+        }
+    }
+}
